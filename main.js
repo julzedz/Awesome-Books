f50027bf3835d6f92e7587d78dcff013d416163e
Edozie <edoziechukwuma@gmail.com>
2022-09-19 22:50:13 +0100

    Set up html,css & js files

---
+++ main.js
@@ -0,0 +1,23 @@
+let collection = [
+  {
+    title: 'Things Fall Apart',
+    author: 'Chinua Achebe',
+  },
+  {
+    title: 'Purple Hibiscus',
+    author: 'Chimamanda Adichie',
+  }
+]
+
+const collectionList = document.querySelector('.collection');
+let allContent = '';
+for (let i = 0; i < collection.length; i += 1) {
+  allContent += `
+    <li class="book">
+      <p class="title">${collection[i].title}</p>
+      <p class="author">${collection[i].author}</p>
+      <button class="remove-btn">Remove</button>
+    </li>
+    <hr>`;
+}
+collectionList.innerHTML = allContent;
