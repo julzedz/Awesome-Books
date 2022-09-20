7b40146d7e1c055745da87021fec3b467c2635c0
Edozie <edoziechukwuma@gmail.com>
2022-09-20 14:54:41 +0100

    Update html & css

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
-let collection = [
-  {
-    title: 'Things Fall Apart',
-    author: 'Chinua Achebe',
-  },
-  {
-    title: 'Purple Hibiscus',
-    author: 'Chimamanda Adichie',
-  }
-]
+const form = document.querySelector('form');
+const collection = document.querySelector('.collection');
+const titleInput = document.querySelector('.title-input');
+const authorInput = document.querySelector('.author-input');
+let books = [];
 
-const collectionList = document.querySelector('.collection');
-let allContent = '';
-for (let i = 0; i < collection.length; i += 1) {
-  allContent += `
-    <li class="book">
-      <p class="title">${collection[i].title}</p>
-      <p class="author">${collection[i].author}</p>
-      <button class="remove-btn">Remove</button>
-    </li>
-    <hr>`;
-}
-collectionList.innerHTML = allContent;
+//create a function for li content
+// function bookInput({title, author}) {
+//   return `<li class="book">
+//       <p class="title">${title}</p>
+//       <p class="author">${author}</p>
+//       <button class="remove-btn">Remove</button>
+//     </li>
+//     <hr>`;
+// }
 
