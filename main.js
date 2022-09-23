81bf9af714eb901e7981fd77f966eee67e46d1ad
Edozie <edoziechukwuma@gmail.com>
2022-09-23 08:28:49 -0600

    Set up export

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-import Books from './booksClass.js';
+import Books from "./booksClass.js";
 
 const collection = document.querySelector('.collection');
 const title = document.querySelector('.title-input');
 const author = document.querySelector('.author-input');
 const addBtn = document.querySelector('.add-btn');
+let newBook = new Books();
+let books = newBook;
 
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
-
+  
   static addBooks() {
     let box = '';
-    const bookLength = Books.books.length;
+    const bookLength = books.length;
     for (let i = 0; i < bookLength; i += 1) {
       box += `<li class="book">
       <div id="space">
-      <p class="title">${Books.books[i].title} by ${Books.books[i].author}</p>
+      <p class="title">${books[i].title} by ${books[i].author}</p>
       <button class="remove-btn">Remove</button>
       </div>
       </li>`;
@@ -26,18 +28,18 @@ class Book {
     this.removeBook();
     this.clearField();
   }
-
+  
   static removeBook() {
     const removeBtn = document.querySelectorAll('.remove-btn');
     removeBtn.forEach((elem, index) => {
       elem.addEventListener('click', () => {
         elem.parentNode.remove();
-        Books.books.splice(index, 1);
-        localStorage.setItem('books', JSON.stringify(Books.books));
+        books.splice(index, 1);
+        localStorage.setItem('books', JSON.stringify(books));
       });
     });
   }
-
+  
   static clearField() {
     document.querySelector('.title-input').value = '';
     document.querySelector('.author-input').value = '';
@@ -48,14 +50,14 @@ addBtn.addEventListener('click', () => {
   const titleValue = title.value;
   const authorValue = author.value;
   const bookObject = new Book(titleValue, authorValue);
-  Books.books.push(bookObject);
+  books.push(bookObject);
   Book.addBooks();
-  localStorage.setItem('books', JSON.stringify(Books.books));
+  localStorage.setItem('books', JSON.stringify(books));
 });
 
 window.addEventListener('load', () => {
   if (localStorage.getItem('books')) {
-    Books.books = JSON.parse(localStorage.getItem('books'));
+    books = JSON.parse(localStorage.getItem('books'));
   }
   Book.addBooks();
 });
