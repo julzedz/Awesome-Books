048f6a9b4b6336157bd7e1f2df0fde97b65e5678
Edozie <edoziechukwuma@gmail.com>
2022-09-23 09:34:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-import Books from "./booksClass.js";
+import Books from './booksClass.js';
 
 const collection = document.querySelector('.collection');
 const title = document.querySelector('.title-input');
 const author = document.querySelector('.author-input');
 const addBtn = document.querySelector('.add-btn');
-let newBook = new Books();
-let books = newBook;
+const newBook = new Books();
+let { books } = newBook;
 
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
+    this.books = JSON.parse(localStorage.getItem('books'));
   }
-  
+
   static addBooks() {
     let box = '';
     const bookLength = books.length;
@@ -20,7 +21,7 @@ class Book {
       box += `<li class="book">
       <div id="space">
       <p class="title">${books[i].title} by ${books[i].author}</p>
-      <button class="remove-btn">Remove</button>
+      <button class="remove-btn" id="${i}">Remove</button>
       </div>
       </li>`;
     }
@@ -28,18 +29,20 @@ class Book {
     this.removeBook();
     this.clearField();
   }
-  
+
   static removeBook() {
     const removeBtn = document.querySelectorAll('.remove-btn');
-    removeBtn.forEach((elem, index) => {
+    removeBtn.forEach((elem) => {
       elem.addEventListener('click', () => {
-        elem.parentNode.remove();
+        elem.parentNode.parentNode.remove();
+        const index = elem.id;
         books.splice(index, 1);
         localStorage.setItem('books', JSON.stringify(books));
+        Book.addBooks();
       });
     });
   }
-  
+
   static clearField() {
     document.querySelector('.title-input').value = '';
     document.querySelector('.author-input').value = '';
