84b135f95d7cc804a64ae36e25bb69f76217461f
Edozie <edoziechukwuma@gmail.com>
2022-09-23 19:35:10 -0600

    Correct linter errors

--- main.js
+++ main.js
@@ -65,21 +65,21 @@ window.addEventListener('load', () => {
   Book.addBooks();
 });
 
-const listPage = document.querySelector('.collection-section')
-const formPage = document.querySelector('.form')
-const contactPage = document.querySelector('.ContactPage')
-const list = document.querySelector(".list-book-nav");
-const addNav = document.querySelector(".add-book-nav");
-const contactNav = document.querySelector(".contact-nav");
-const date = document.querySelector(".date");
+const listPage = document.querySelector('.collection-section');
+const formPage = document.querySelector('.form');
+const contactPage = document.querySelector('.ContactPage');
+const list = document.querySelector('.list-book-nav');
+const addNav = document.querySelector('.add-book-nav');
+const contactNav = document.querySelector('.contact-nav');
+const date = document.querySelector('.date');
 
 list.addEventListener('click', () => {
   listPage.style.display = 'block';
   formPage.style.display = 'none';
   contactPage.style.display = 'none';
   list.classList.add('displaye');
-  addNav.classList.remove('displaye')
-  contactNav.classList.remove('displaye')
+  addNav.classList.remove('displaye');
+  contactNav.classList.remove('displaye');
 });
 
 addNav.addEventListener('click', () => {
@@ -87,17 +87,17 @@ addNav.addEventListener('click', () => {
   listPage.style.display = 'none';
   contactPage.style.display = 'none';
   list.classList.remove('displaye');
-  addNav.classList.add('displaye')
-  contactNav.classList.remove('displaye')
+  addNav.classList.add('displaye');
+  contactNav.classList.remove('displaye');
 });
 
 contactNav.addEventListener('click', () => {
   contactPage.style.display = 'block';
   listPage.style.display = 'none';
   formPage.style.display = 'none';
-  contactNav.classList.add('displaye')
+  contactNav.classList.add('displaye');
   list.classList.remove('displaye');
-  addNav.classList.remove('displaye')
+  addNav.classList.remove('displaye');
 });
 
-document.querySelector(".date").innerHTML = Date().toLocaleString();
+date.innerHTML = new Date().toLocaleString();
