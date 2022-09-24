9fd4a0ddd57440913e44ea90ee1ebf7063aa8a9d
Edozie <edoziechukwuma@gmail.com>
2022-09-23 18:51:32 -0600

    Update page

--- main.js
+++ main.js
@@ -77,17 +77,25 @@ list.addEventListener('click', () => {
   listPage.style.display = 'block';
   formPage.style.display = 'none';
   contactPage.style.display = 'none';
+  list.classList.add('displaye');
+  addNav.classList.remove('displaye')
+  contactNav.classList.remove('displaye')
 });
 
 addNav.addEventListener('click', () => {
   formPage.style.display = 'block';
   listPage.style.display = 'none';
   contactPage.style.display = 'none';
+  list.classList.remove('displaye');
+  addNav.classList.add('displaye')
+  contactNav.classList.remove('displaye')
 });
 
 contactNav.addEventListener('click', () => {
   contactPage.style.display = 'block';
   listPage.style.display = 'none';
   formPage.style.display = 'none';
+  contactNav.classList.add('displaye')
+  list.classList.remove('displaye');
+  addNav.classList.remove('displaye')
 });
-console.log(contactPage)
